717359f02bfa2a582b9b39043e726019ced2c9ac
Pantai Suyasri <pantaisuyasri@gmail.com>
2023-08-20 19:32:55 +0700

    help

--- app.py
+++ app.py
@@ -1,40 +1,51 @@
 import os
 from typing import Optional, Tuple
-
 import gradio as gr
-from langchain.chains import ConversationChain
-from langchain.llms import OpenAI
 from threading import Lock
-import openai
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+# os.envrion["OPENAI_API_KEY"] = "sk-..."  # Replace with your key
+
+embeddings = OpenAIEmbeddings()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
 
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
+def predict(message, history):
+    chat_history = []
+    for human, ai in history:
+        chat_history.append(HumanMessage(content=human))
+        chat_history.append(AIMessage(content=ai))
+    if chain:
+        response = chain({"question": message, "chat_history": chat_history}).content
+    else:
+        response = "Please input a document"
+    chat_history.append(HumanMessage(content=message))
+    return response
+
+
+def input_url(url_string: str):
+    urls = list(map(str.strip, url_string.split(",")))
+    loader = UnstructuredURLLoader(urls=urls)
+    data = loader.load()
+    documents = text_splitter.split_documents(data)
+    vectorstore = Chroma.from_documents(documents, embeddings)
+    chain = ConversationalRetrievalChain.from_llm(
+        ChatOpenAI(), vectorstore.as_retriever()
+    )
     return chain
 
-
-def set_openai_api_key(api_key: str):
-    """Set the api key and return chain.
-
-    If no api_key, then None is returned.
-    """
-    if api_key:
-        os.environ["OPENAI_API_KEY"] = api_key
-        chain = load_chain()
-        return chain
-    else:
-        if os.getenv("OPENAI_API_KEY"):
-            chain = load_chain()
-            return chain
-
 class ChatWrapper:
-
     def __init__(self):
         self.lock = Lock()
     def __call__(
-        self, api_key: str, inp: str, history: Optional[Tuple[str, str]], chain: Optional[ConversationChain]
+        self, api_key: str, inp: str, history, chain
     ):
         """Execute the chat functionality."""
         self.lock.acquire()
@@ -56,42 +67,25 @@ class ChatWrapper:
             self.lock.release()
         return history, history
 
-chat = ChatWrapper()
 
 
-with gr.Blocks() as block:
+
+with gr.Blocks() as app:
+    chain = gr.State(None)
     with gr.Row():
-        gr.Markdown("<h3><center>LangChain Demo</center></h3>")
+        with gr.Tab("Upload file"):
+            gr.Markdown("Upload your document here. Must be a PDF.")
+            file_input = gr.File()
+        with gr.Tab("Input URL"):
+            gr.Markdown("Input your URL here. For multiple pages, seperate by comma.")
+            with gr.Row():
+                url_input_textbox = gr.Textbox(
+                    placeholder="https://en.wikipedia.org/wiki/Doge_(meme)",
+                    show_label=False,
+                )
+                url_submit_button = gr.Button("Submit", scale=0)
+    gr.ChatInterface(predict)
 
-    chatbot = gr.Chatbot()
+    url_submit_button.click(input_url, inputs=[url_input_textbox], outputs=[chain])
 
-    with gr.Row():
-        message = gr.Textbox(
-            label="What's your question?",
-            placeholder="What's the answer to life, the universe, and everything?",
-            lines=1,
-        )
-        submit = gr.Button(value="Send", variant="secondary").style(full_width=False)
-
-    gr.Examples(
-        examples=[
-            "Hi! How's it going?",
-            "What should I do tonight?",
-            "Whats 2 + 2?",
-        ],
-        inputs=message,
-    )
-
-    state = gr.State()
-    agent_state = gr.State()
-
-    submit.click(chat, inputs=[openai_api_key_textbox, message, state, agent_state], outputs=[chatbot, state])
-    message.submit(chat, inputs=[openai_api_key_textbox, message, state, agent_state], outputs=[chatbot, state])
-
-    openai_api_key_textbox.change(
-        set_openai_api_key,
-        inputs=[openai_api_key_textbox],
-        outputs=[agent_state],
-    )
-
-block.launch(debug=True)
+app.launch()
